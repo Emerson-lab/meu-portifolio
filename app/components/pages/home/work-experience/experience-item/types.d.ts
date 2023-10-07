9810b2c5d7ba08c1e89b362d0ed1c975bae522e1
Emerson Trindade <trinddade29@gmail.com>
2023-10-06 22:27:30 -0300

    feat: WorkExperience integration page

---
+++ app/components/pages/home/work-experience/experience-item/types.d.ts
@@ -0,0 +1,5 @@
+import { WorkExperienceType } from "@/app/Types/work-experience"
+
+export type ExperienceItemProps = {
+  expirencie: WorkExperienceType
+}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,32 @@
-import { PageIntroduction } from "../components/pages/projects/page-introduction";
+import { REVALIDATE_DATA } from "../variables";
+import { fetchHygraphQuery } from "../utils/fetch-hygraph-query";
 import { ProjectList } from "../components/pages/projects/projects-list";
+import { PageIntroduction } from "../components/pages/projects/page-introduction";
+
+const getPageData = async () => {
+  const query = `
+    query ProjectsQuery {
+      projects {
+        shortDescription
+        slug
+        title
+        thumbnail {
+          url
+        }
+        technologies {
+          name
+        }
+      }
+    }
+  `
+  return fetchHygraphQuery(query, REVALIDATE_DATA);
+}
 
 export default function Projects() {
   return (
     <>
-      <PageIntroduction/>
-      <ProjectList/>
+      <PageIntroduction />
+      <ProjectList />
     </>
   )
 }
